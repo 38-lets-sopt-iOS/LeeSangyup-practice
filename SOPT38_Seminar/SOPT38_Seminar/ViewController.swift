63df9e5db8b5f84d7a00372b8d5137919166c894
sangyup <sangyeobi712@gmail.com>
2026-04-07 16:57:29 +0900

    new Project

---
+++ SOPT38_Seminar/SOPT38_Seminar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOPT38_Seminar
+//
+//  Created by 이상엽 on 4/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
