f2b65a27594dffce8c9c072ae133b17ef67cb64a
sangyup <sangyeobi712@gmail.com>
2026-04-09 14:38:54 +0900

    [Style] #1 - UIFont+ 추가

--- SOPT38_Seminar/SOPT38_Seminar/Extension/UIFont+.swift
+++ SOPT38_Seminar/SOPT38_Seminar/Extension/UIFont+.swift
@@ -5,4 +5,34 @@
 //  Created by 이상엽 on 4/9/26.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    class var head1: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 30)!
+    }
+    
+    class var subhead1: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 18)!
+    }
+    
+    class var subhead2: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 16)!
+    }
+    
+    class var subhead3: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 15)!
+    }
+    
+    class var subhead4: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 14)!
+    }
+    
+    class var subhead5: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 14)!
+    }
+    
+    class var info12: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 12)!
+    }
+}
