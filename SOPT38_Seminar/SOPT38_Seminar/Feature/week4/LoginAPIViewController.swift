612dbf9ba5cd5adaeeec916647440b42eb0915b0
sangyup <sangyeobi712@gmail.com>
2026-05-06 19:11:38 +0900

    [#6] Signup UI 구현

---
+++ SOPT38_Seminar/SOPT38_Seminar/Feature/week4/LoginAPIViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginAPIViewController.swift
+//  SOPT38_Seminar
+//
+//  Created by 이상엽 on 5/6/26.
+//
+
+import UIKit
+
+final class LoginAPIViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
